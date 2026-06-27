0da2e0fbd7ebbb1e7be977fadef1619c2a43f7e5
Anderson Nieves <andersonvnieves@live.com>
2026-06-27 12:05:13 -0300

    Add arquivos

---
+++ br.com.fiap.cloudgames.Payment.Application/Services/IPaymentProcessorService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace br.com.fiap.cloudgames.Payment.Application.Services
+{
+    internal interface IPaymentProcessorService
+    {
+    }
+}
---
+++ br.com.fiap.cloudgames.Payment.Domain/Aggregates/Payment.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace br.com.fiap.cloudgames.Payment.Domain.Aggregates
+{
+    internal class Payment
+    {
+        //PaymentId
+        //OrderId (em caso de extorno, segundo pagamento ou transação mas mesmo pedido)
+        //Status: Pending, Approved, Denied
+    }
+}
